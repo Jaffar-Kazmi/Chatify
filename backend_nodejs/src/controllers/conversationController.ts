5954118352feef12b0e6047d01c8f9dc6477a6cf
Jaffar kazmi <kazmijaffar890@gmail.com>
2025-11-26 03:51:58 +0500

    feat(conversations): fetch all Chats from DB

--- backend_nodejs/src/controllers/conversationController.ts
+++ backend_nodejs/src/controllers/conversationController.ts
@@ -10,19 +10,27 @@ export const fetchAllConversationsByUserId = async (req: Request, res: Response)
     try{
         const result = await pool.query(
             `
-            SELECT c.id AS conversation_id, u.username AS participant_name, m.content AS last_message, m.created_at AS last_message_time
-            FROM conversations c 
-            JOIN users u ON (u.id = c.participant_two AND u.id != $1)
+            SELECT 
+                c.id AS conversation_id,
+                CASE 
+                    WHEN u1.id = $1 THEN u2.username
+                    ELSE u1.username
+                END AS participant_name,
+                m.content AS last_message,
+                m.created_at AS last_message_time
+            FROM conversations c
+            JOIN users u1 ON u1.id = c.participant_one
+            JOIN users u2 ON u2.id = c.participant_two
             LEFT JOIN LATERAL (
                 SELECT content, created_at
                 FROM messages
                 WHERE conversation_id = c.id
-                ORDER BY created_at DESC
-                LIMIT 1
+                ORDER by created_at desc
+                LIMIT 1            
             ) m ON true
-             WHERE  c.participant_one = $1 OR c.participant_two = $1
-             ORDER BY m.created_at DESC
-             `,
+            WHERE c.participant_one = $1 or c.participant_two = $1
+            ORDER by m.created_at DESC;            
+            `,
              [userId]            
         );
 
