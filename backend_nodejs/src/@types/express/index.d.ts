ac9f31d8ace1a280b919f80ee3b756106ca28e03
Jaffar kazmi <kazmijaffar890@gmail.com>
2025-12-19 05:57:10 +0500

    fix: fix type

--- backend_nodejs/src/@types/express/index.d.ts
+++ backend_nodejs/src/@types/express/index.d.ts
@@ -1,5 +1,9 @@
-declare namespace Express {
-  export interface Request {
-    user?: any; 
+import * as express from "express";
+
+declare global {
+  namespace Express {
+    interface Request {
+      user?: any;
+    }
   }
 }
