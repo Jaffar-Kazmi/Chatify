c0bd2a6bdd53bae3a2df33ae05e17330aae34a66
Jaffar kazmi <kazmijaffar890@gmail.com>
2025-12-19 23:06:24 +0500

    fix: add file property to AuthRequest

--- backend_nodejs/src/controllers/profileController.ts
+++ backend_nodejs/src/controllers/profileController.ts
@@ -5,6 +5,7 @@ const baseUrl = process.env.BASE_URL;
 
 interface AuthRequest extends Request {
   user?: { id: string };
+  file?: Express.Multer.File;
 }
 
 export const getProfile = async (req: Request, res: Response) => {
